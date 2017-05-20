3ae00cf8ee54eefcd19a3c7234adfb1d36d8c467
Brian151 <Brian151@users.noreply.github.com>
2017-05-19 19:44:29 -0600

    Slowly but surely getting parser working again

--- tools/Format/shockwave_haxe/shockwaveparser/bin/AppMain.js
+++ tools/Format/shockwave_haxe/shockwaveparser/bin/AppMain.js
@@ -1,6 +1,20 @@
-(function (console) { "use strict";
+(function (console, $hx_exports) { "use strict";
+var brian151 = $hx_exports.brian151 = $hx_exports.brian151 || {};
+$hx_exports.brian151.riff = $hx_exports.brian151.riff || {};
+;$hx_exports.brian151.earthquake = $hx_exports.brian151.earthquake || {};
+$hx_exports.brian151.earthquake.filesystem = $hx_exports.brian151.earthquake.filesystem || {};
 var brian151_Main = function() { };
 brian151_Main.main = function() {
+	brian151_Main.test = false;
+	brian151_Main.xhr = new XMLHttpRequest();
+	brian151_Main.xhr.responseType = "arraybuffer";
+	brian151_Main.xhr.open("GET","spybot_0807_sw.dir",true);
+	brian151_Main.xhr.send();
+	brian151_Main.xhr.onreadystatechange = function() {
+		if(brian151_Main.xhr.readyState == 4) {
+			if(brian151_Main.xhr.status == 200) brian151_Main.mov = new brian151.earthquake.Movie(brian151_Main.xhr.response);
+		}
+	};
 };
 brian151_Main.main();
-})(typeof console != "undefined" ? console : {log:function(){}});
+})(typeof console != "undefined" ? console : {log:function(){}}, typeof $hx_scope != "undefined" ? $hx_scope : $hx_scope = {});
--- tools/Format/shockwave_haxe/shockwaveparser/src/brian151/Main.hx
+++ tools/Format/shockwave_haxe/shockwaveparser/src/brian151/Main.hx
@@ -1,5 +1,8 @@
 package brian151;
+import brian151.earthquake.Movie;
 import js.Lib;
+import js.html.XMLHttpRequest;
+import js.Browser;
 
 /**
  * ...
@@ -8,10 +11,22 @@ import js.Lib;
 
 class Main 
 {
-	
-	static function main() 
-	{
-		
+	static var mov:Movie;
+	static var xhr:XMLHttpRequest;
+	static var test:Bool;
+	static function main() {
+		test = false;
+		xhr = new XMLHttpRequest();
+		untyped __js__("brian151_Main.xhr.responseType = \"arraybuffer\"");
+		xhr.open("GET", "spybot_0807_sw.dir", true);
+		xhr.send();
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+					mov = new Movie(xhr.response);
+				}
+			}
+		}
 	}
 	
 }
