c3a1bc428f364c6a1a7e03cd1b87149b606659ef
CATERPILLAR\Braxen <braxen@braxnet.org>
2017-07-03 11:00:12 +0200

    some bitmap changes, sound cue points
    make some ghetto fix to properly extract bitmaps with different
    encodings and whatnot, and found out that cupt = cue point files,
    extract that into json metadata because wav doesn't natively support it,
    and writing data to aiff doesn't seem to work all that well

--- tools/cst_python.py
+++ tools/cst_python.py
@@ -10,6 +10,8 @@ import struct
 
 import wave
 
+import aifc
+
 import ntpath
 
 import json
@@ -29,6 +31,20 @@ def convertBITD( w, h, f, entry ):
 	start = entry['offset'] + 8 # +8 bytes to skip fourcc and length
 	size = entry['length']
 
+	readMode = 0
+
+	if entry["bitdepth"] > 8: # this is bad, there's actually no value if it's a 1-bit image, so it's reading the next byte instead, but it works so hey
+		readMode = 1
+
+	pad = 0
+	if w % 2:
+		pad = h
+
+	if ( ( w * h ) + pad ) == size:
+		readMode = 2
+
+	print("readMode: " + str(readMode))
+
 	#padbytes = -1
 
 	# seek to the bitd image data
@@ -36,7 +52,7 @@ def convertBITD( w, h, f, entry ):
 
 	while f.tell() <= start + size:
 
-		if entry["bitdepth"] > 8: # this is bad, there's actually no value if it's a 1-bit image, so it's reading the next byte instead, but it works so hey
+		if readMode == 1: 
 
 			msk = f.read(1)
 
@@ -59,11 +75,32 @@ def convertBITD( w, h, f, entry ):
 
 				i += 1
 
+		elif readMode == 2:
+
+			col = struct.unpack('B', f.read(1) )[0]
+
+			bitmapValues[ draw_y ][ draw_x ] = 0xFF - col
+
+			draw_x += 1
+
+			if draw_x >= w:
+				# print("reached line end at " + str( f.tell() - start ) )
+				if pad:
+					f.read(1) # padding byte?
+				
+				draw_x = 0
+				draw_y += 1
+
+			if draw_y >= h:
+				print("reached end at " + str( f.tell() - start ) )
+				return bitmapValues
+
 		else:
 
 			rLen = struct.unpack('B', f.read(1) )[0]
 
-			if 0x101 - rLen > 0x7F: # this is interesting, for some bitmaps it works, but some need 0x100
+			if 0x100 - rLen > 0x7F:
+				#if 0x101 - rLen > 0x7F: # this is interesting, for some bitmaps it works, but some need 0x100
 				
 				#doLog("   lin (" + str(draw_x) + "," + str(draw_y) + " - len " + str(rLen) + ")" )
 
@@ -134,9 +171,10 @@ def convertBITD( w, h, f, entry ):
 						#doLog("    rle - exceeded height (" + str( (start+size) - f.tell() ) + " bytes left)")
 						return bitmapValues
 
+
 	return bitmapValues
 
-
+writeRaw = False
 
 fileNum = 1
 
@@ -311,9 +349,11 @@ def readCST(f):
 				doLog("  INVALID KEY FILE SLOT: " + str( castFileSlot ) + "->" + str( castSlot ) + " (" + str( castType ) + ") @ " + str(kPos) )
 
 			else:
+				entries[ castFileSlot ]['parent'] = entries[ castSlot ]
 				entries[ castSlot ]['files'].append( entries[ castFileSlot ] )
 			
 			# doLog("  KeyCastOffset: " + str( castOffset ) + ", KeyCastId: " + str( castId ) + ", KeyCastType: " + str( castType ) )
+
 
 
 	# loop through all the rest of the files
@@ -367,6 +407,75 @@ def readCST(f):
 		if e['type'] == 'sndS':
 			e['content'] = f.read(fEntryLength)
 
+
+		if e['type'] == 'sndH':
+			# e['content'] = f.read(fEntryLength)
+
+			f.read(4) # unknown
+
+			soundLength = struct.unpack('>i', f.read(4) )[0]
+
+			f.read(4) # what
+
+			f.read(20) # null?
+
+			f.read(4) # sound length plus what
+
+			f.read(4) # sound length again?
+
+			f.read(4) # sound length again?
+
+			sampleRate = struct.unpack('>i', f.read(4) )[0]
+
+			f.read(4) # sample rate again?
+
+			e['soundLength'] = soundLength
+			e['sampleRate'] = sampleRate
+
+			if 'parent' in e:
+				e['parent']['soundLength'] = soundLength
+				e['parent']['sampleRate'] = sampleRate
+
+
+		if e['type'] == 'cupt':
+
+			# e['content'] = f.read(fEntryLength)
+			# f.seek( e['offset'], 0 )
+
+			cuptEntries = struct.unpack('>i', f.read(4) )[0]
+
+			# print("CUPT Entries: " + str(cuptEntries))
+
+			e['cuePoints'] = []
+
+			for i in range(0, cuptEntries):
+
+				something = struct.unpack('>h', f.read(2) )[0]
+
+				sampleOffset = struct.unpack('>h', f.read(2) )[0]
+
+				textLength = struct.unpack('>b', f.read(1) )[0]
+
+				# print("TL: " + str(textLength))
+
+				if textLength > 0:
+
+					cueName = f.read(textLength).decode("ansi")
+
+				else:
+
+					cueName = ""
+
+
+				padLength = 31 - textLength
+
+				f.seek(padLength, 1)
+
+				e['cuePoints'].append([ sampleOffset, cueName ])
+
+				print( " " + str(cueName) + " @ " + str(sampleOffset) + "/" + str(something) )
+
+
 		
 		if e['type'] == 'CASt':
 
@@ -385,31 +494,39 @@ def readCST(f):
 			# bitmap
 			if castType == 1:
 
-				f.read(32) # it always seems to be 32, skip that
+				# f.read(2) # it always seems to be 32, skip that
 
-				# this byte appears to be 02 if there's a name available
-				hasName = struct.unpack('>h', f.read(2) )[0]
+				something = struct.unpack('>h', f.read(2) )[0]
 
-				if hasName > 0:
+				if something == 0:
 
-					f.read(8) # pad
+					f.read(30)
 
-					f.read(4) # text length int32, not required i think
+					# this byte appears to be 02 if there's a name available
+					hasName = struct.unpack('>h', f.read(2) )[0]
 
-					# read cast name with a byte infront specifying the length
-					castInfoName = f.read( struct.unpack('b', f.read(1) )[0] ).decode('ansi')
-					e['name'] = castInfoName
+					if hasName > 0:
 
-				else:
+						f.read(8) # pad
 
-					doLog(" !!! BITMAP - NO NAME (" + str( e['memberNum'] ) + ") !!!")
+						f.read(4) # text length int32, not required i think
+
+						# read cast name with a byte infront specifying the length
+						castInfoName = f.read( struct.unpack('b', f.read(1) )[0] ).decode('ansi')
+						e['name'] = castInfoName
+
+						# f.read(3)
+
+					else:
+
+						doLog(" !!! BITMAP - NO NAME (" + str( e['memberNum'] ) + ") !!!")
+
+						f.read(3)
 
 					f.read(3)
 
 
-				e['friendlyType'] = 'bitmap'
-
-				f.read(3)
+				e['friendlyType'] = 'bitmap'				
 
 				# i don't know the term for this, so i called it padding
 				e['paddingH'] = struct.unpack('>h', f.read(2) )[0]
@@ -485,7 +602,17 @@ def readCST(f):
 					f.read(4)
 				'''
 
-				f.read(100)
+				skipLen = struct.unpack('>i', f.read(4) )[0]
+
+				f.read(skipLen - 4)
+
+				cFields = struct.unpack('>h', f.read(2) )[0]
+
+				print("fields: " + str(cFields))
+
+				for i in range(0, cFields):
+					f.read(4)
+
 
 				castInfoLen = struct.unpack('>i', f.read(4) )[0]
 
@@ -533,13 +660,13 @@ def parseCast( num, f ):
 	
 	# doLog("[CAST " + str(e['num']) + "]")
 
-	global entries, castList, metaList
+	global entries, castList, metaList, writeRaw
 
 	# e = entries[num]
 	e = castList[ num ]
 
 	if e['type'] != 'CASt':
-		doLog("[" + e['type'] + "] ???\n")
+		# doLog("[" + e['type'] + "] ???\n")
 		return False
 
 	metaList[ e['memberNum'] ] = {}
@@ -557,31 +684,30 @@ def parseCast( num, f ):
 		doLog(" [CODEC] " + str( e["codec"] ) )
 		metaList[ e['memberNum'] ]['soundCodec'] = e['codec']
 
+	if e["castType"] == 1: # bitmap
 
-	if 'width' in e:
 		doLog(" [SIZE] " + str( e["width"] ) + "x" + str( e["height"] ) + " (" + str( e["widthRaw"] ) + "x" + str( e["heightRaw"] ) + ")" )
 		metaList[ e['memberNum'] ]['imageWidth'] = e['width']
 		metaList[ e['memberNum'] ]['imageHeight'] = e['height']
 
-	if 'paddingW' in e:
 		doLog(" [PAD] " + str( e["paddingW"] ) + "x" + str( e["paddingH"] ) )
 
-	if 'regx' in e:
 		doLog(" [REG] " + str( e["regx"] ) + "," + str( e["regy"] ) )
 		metaList[ e['memberNum'] ]['imageRegX'] = e['regx']
 		metaList[ e['memberNum'] ]['imageRegY'] = e['regy']
 
-	if 'constant' in e:
 		doLog(" [CONSTANT] " + str( e["constant"] ) + " / " + str( struct.unpack('i', e["constant"] )[0] ) )
 
-	if 'bitdepth' in e:
 		doLog(" [BITDEPTH] " + str( e["bitdepth"] ) )
 
-	if 'bitalpha' in e:
 		doLog(" [BITALPHA] " + str( e["bitalpha"] ) )
 
-	if 'palette' in e:
 		doLog(" [PALETTE] " + str( e["palette"] ) )
+
+	if e["castType"] == 6: # sound
+
+		doLog(" [SAMPLERATE] " + str( e["sampleRate"] ) )
+		metaList[ e['memberNum'] ]['sampleRate'] = e['sampleRate']
 
 
 	doLog(" [SYS] POffset: " + str( e["poffset"] ) + ", Offset: " + str( e["offset"] ) + ", Length: " + str( e["length"] ) + ", Data length: " + str( e["dataLength"] ) + ", Data end: " + str( e["dataEnd"] ) )
@@ -598,14 +724,42 @@ def parseCast( num, f ):
 		doLog("  [LINKED] Num: " + str(l["num"]) + ", Type: " + l['type'] + ", POffset: " + str( l['poffset'] ) + ", Offset: " + str( l['offset'] ) + ", Length: " + str( l['length'] ) )
 
 		if l['type'] == "sndS":
+
+			# if e['codec'] == "kMoaCfFormat_WAVE" or e['codec'] == "kMoaCfFormat_snd":
 			
 			# using a python library here to write the wav data, even if it's an aiff file, dunno why that works
 			snd = wave.open( outFolder + "/" + str(e['memberNum']) + ".wav", "w")
 			snd.setnchannels(1)
 			snd.setsampwidth(1)
-			snd.setframerate(22050.0)
-			snd.writeframesraw( l['content'] )
+			snd.setframerate( e["sampleRate"] )
+			snd.writeframes( l['content'] )
 			snd.close()
+
+			'''
+			elif e['codec'] == "kMoaCfFormat_AIFF":
+			
+				# using a python library here to write the wav data, even if it's an aiff file, dunno why that works
+				snd = aifc.open( outFolder + "/" + str(e['memberNum']) + ".aifc", "w")
+
+				# snd.aiff()
+				
+				snd.setnchannels(1)
+
+				snd.setsampwidth(1)
+
+				snd.setframerate( e["sampleRate"] )
+
+				snd.writeframes( l['content'] )
+
+				snd.close()
+			'''
+
+
+			doLog("   [DURATION] " + str( l["length"] / e["sampleRate"] ) )
+
+			metaList[ e['memberNum'] ]['duration'] = l["length"] / e["sampleRate"]
+
+			metaList[ e['memberNum'] ]['size'] = l["length"]
 			
 
 		if l['type'] == "BITD":
@@ -617,13 +771,15 @@ def parseCast( num, f ):
 				continue
 
 			l["bitdepth"] = e["bitdepth"]
+
+			metaList[ e['memberNum'] ]['size'] = l["length"]
 			
-			'''
-			# uncomment to write raw files
-			bitm = open( outFolder + "/" + str(e['memberNum']) + ".bitd", "wb")
-			bitm.write( l['content'] )
-			bitm.close()
-			'''
+			
+			if writeRaw:
+				bitm = open( outFolder + "/" + str(e['memberNum']) + ".bitd", "wb")
+				bitm.write( l['content'] )
+				bitm.close()
+			
 
 			if e["bitdepth"] > 8:
 				im = Image.new("1", (e["width"], e["height"]) ) # 1-bit 0/1 image
@@ -648,11 +804,12 @@ def parseCast( num, f ):
 					dr.point( (x, y), bitmapValues[y][x] )
 
 			# save as bmp
-			im.save( outFolder + "/" + str(e['memberNum']) + ".bmp", "BMP")
+			im.save( outFolder + "/tmp.bmp", "BMP")
 
 			# use magick to convert one opaque and one transparent (from white colour) png
-			call("magick convert " + outFolder + "/" + str(e['memberNum']) + ".bmp " + outFolder + "/" + str(e['memberNum']) + "O.png")
-			call("magick convert " + outFolder + "/" + str(e['memberNum']) + ".bmp -transparent \"#FFFFFF\" " + outFolder + "/" + str(e['memberNum']) + "T.png")
+			call("magick convert " + outFolder + "/tmp.bmp " + outFolder + "/" + str(e['memberNum']) + ".png")
+			# call("magick convert " + outFolder + "/tmp.bmp " + outFolder + "/" + str(e['memberNum']) + "O.png")
+			# call("magick convert " + outFolder + "/tmp.bmp -transparent \"#FFFFFF\" " + outFolder + "/" + str(e['memberNum']) + "T.png")
 			
 			del dr
 
@@ -663,13 +820,23 @@ def parseCast( num, f ):
 			txt.write( l['content'] )
 			txt.close()
 			
-	'''
-	# uncomment to write raw files
-	cst = open( outFolder + "/" + str(e['memberNum']) + ".cast", "wb")
-	f.seek( e['offset'], 0 )
-	cst.write( f.read( e['length'] + 8 ) )
-	cst.close()
-	'''
+
+		if l['type'] == "cupt":
+
+			print("   [CUEPOINTS] " + str(l['cuePoints']) )
+
+			metaList[ e['memberNum'] ]['cuePoints'] = l['cuePoints']
+
+			# cupt = open( outFolder + "/" + str(e['memberNum']) + ".cupt", "wb")
+			# cupt.write( l['content'] )
+			# cupt.close()
+	
+	if writeRaw:
+		cst = open( outFolder + "/" + str(e['memberNum']) + ".cast", "wb")
+		f.seek( e['offset'], 0 )
+		cst.write( f.read( e['length'] + 8 ) )
+		cst.close()
+	
 
 	# test
 	if e["castType"] == 4:
